1845a425231d22c2446d5a38903ad3f50e0d72fd
Ashwanth2 <kaviashwanth344@gmail.com>
2023-07-05 23:48:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function CRUD() {
+    const list = [
+        {
+            id: 1, 
+            name: "eashwar",
+            role: "Teacher"
+        },
+        {
+            id: 2, 
+            name: "sanjay",
+            role: "student"
+        },
+    ]
+    const [lists, setList] = useState(list)
+    const [updateState, setUpdateState] = useState(-1)
+    return(
+        <div className='crud'>
+            <div>
+            <AddList setList = {setList }/>
+            <form onSubmit={handleSubmit}>
+            <table>
+                {
+                    lists.map((current) => (
+                        updateState === current.id ? <EditList current={current} lists={lists} setList={setList}/> :
+                        <tr>
+                            <td>{current.name}</td>
+                            <td>{current.role}</td>
+                            <td>
+                                <button className='edit' onClick={() => handleEdit(current.id)}>Edit</button>
+                                <button className='delete' type='button' onClick={() => handleDelete(current.id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </table>
+            </form>
+            </div>
+        </div>
+    )
+
+    function handleEdit(id) {
+        setUpdateState(id)
+    }
+    function handleDelete(id) {
+        const newlist = lists.filter((li) => li.id !== id)
+        setList(newlist)
+    }
+    function handleSubmit(event) {
+        event.preventDefault()
+        const name = event.target.elements.name.value
+        const role = event.target.elements.role.value
+        const newlist = lists.map((li) => (
+            li.id === updateState ? {...li, name:name, role: role} : li
+        ))
+
+        setList(newlist)
+        setUpdateState(-1)
+    }
 }
 
-export default App;
+function EditList({current, lists, setList}) {
+    function handInputname(event) {
+        const value = event.target.value;
+        const newlist = lists.map((li) => (
+            li.id === current.id ? {...li, name :value} : li
+        ))
+
+        setList(newlist)
+    }
+    function handInputrole(event) {
+        const value = event.target.value;
+        const newlist = lists.map((li) => (
+            li.id === current.id ? {...li, role :value} : li
+        ))
+
+        setList(newlist)
+    }
+    return(
+        <tr>
+            <td><input type="text" onChange={handInputname} name='name' value={current.name}/></td>
+            <td><input type="text" onChange={handInputrole} name='role' value={current.role}/></td>
+            <td><button type='submit'>Update</button></td>
+        </tr>
+    )
+}
+
+function AddList({setList}) {
+    const nameRef = useRef()
+    const roleRef = useRef()
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const name = event.target.elements.name.value;
+        const role = event.target.elements.role.value;
+        const newlist = {
+            id: 3,
+            name,
+            role
+        }
+        setList((prevList)=> {
+            return prevList.concat(newlist)
+        })
+        nameRef.current.value = ""
+        roleRef.current.value = ""
+    }
+    return(
+        <form className='addForm' onSubmit={handleSubmit}>
+            <input type="text" name="name" placeholder="Enter Name" ref={nameRef}/>
+            <input type="text" name="role" placeholder="Enter role" ref={roleRef}/>
+            <button type="submit">Add</button>
+        </form>
+    )
+}
+
+export default CRUD;
